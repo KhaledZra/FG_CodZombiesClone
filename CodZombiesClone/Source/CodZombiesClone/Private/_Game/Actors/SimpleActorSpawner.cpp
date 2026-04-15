abe3e6491f4569f885f2b6138a82c7c21c1bd2fe
Khaled.Zraiqi <halo_434000@hotmail.com>
2026-04-15 11:52:27 +0200

    Got a simple zombie spawner going. Causes warnings for now.

---
+++ CodZombiesClone/Source/CodZombiesClone/Private/_Game/Actors/SimpleActorSpawner.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "_Game/Actors/SimpleActorSpawner.h"
+
+
+// Sets default values
+ASimpleActorSpawner::ASimpleActorSpawner()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASimpleActorSpawner::SpawnActor(AActor* OldSpawnedActor)
+{
+	if (ActorClassToSpawn == nullptr) return;
+	// if (OldSpawnedActor != nullptr) return; // Not dead?
+	
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.Owner = this;
+	CurrentSpawnedActor = GetWorld()->SpawnActor<AActor>(ActorClassToSpawn, GetActorLocation(), FRotator::ZeroRotator,
+	                                                     SpawnParameters);
+	CurrentSpawnedActor->OnDestroyed.AddDynamic(this, &ASimpleActorSpawner::SpawnActor);
+	// todo: fix Context Requirements in UStateTreeComponent::StopLogic failed. Component tick is disabled.
+}
+
+// Called when the game starts or when spawned
+void ASimpleActorSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnActor(nullptr);
+}
---
+++ CodZombiesClone/Source/CodZombiesClone/Public/_Game/Actors/SimpleActorSpawner.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SimpleActorSpawner.generated.h"
+
+UCLASS()
+class CODZOMBIESCLONE_API ASimpleActorSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASimpleActorSpawner();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> ActorClassToSpawn;
+
+	UPROPERTY(visibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<AActor> CurrentSpawnedActor;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnActor(AActor* OldSpawnedActor);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
